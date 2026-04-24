b3b4ede358b33155e0411a994b762d06c09aa89b
How <clicoc@users.noreply.github.com>
2026-04-24 22:14:23 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ export const config = {
 export default async function handler(req) {
   const url = new URL(req.url);
   // 从环境变量读取基础地址，如果没有则使用默认值
-  const base = process.env.backendurl;
+  const base = process.env.BACKEND_URL;
   
   // 构建新的目标 URL
   const targetUrl = `${base}${url.pathname}${url.search}`;
